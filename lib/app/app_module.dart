a8986c2075efb32f06fcdab0a40879383fbfc534
João Pedro <joao_giacometti@hotmail.com>
2021-03-19 22:39:41 -0300

    Slidy Start

---
+++ lib/app/app_module.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:film_app_lab/app/app_widget.dart';
+import 'package:film_app_lab/app/modules/home/home_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRouter> get routers => [
+        ModularRouter(Modular.initialRoute, module: HomeModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
---
+++ lib/app/modules/home/home_controller.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+part 'home_controller.g.dart';
+
+@Injectable()
+class HomeController = _HomeControllerBase with _$HomeController;
+
+abstract class _HomeControllerBase with Store {
+  @observable
+  int value = 0;
+
+  @action
+  void increment() {
+    value++;
+  }
+
+}
